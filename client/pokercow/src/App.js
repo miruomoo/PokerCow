b837a13da46738e1ccba511147c849721331ac01
Miruo Lin <miruomoo@gmail.com>
2024-02-08 12:48:14 -0500

    Established socket connection

--- client/pokercow/src/App.js
+++ client/pokercow/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  function sendMessage(){
+    socket.emit("send_message", {message});
+  }
+
+  useEffect(()=>{
+    socket.on("receive_message", (data)=>{
+      setMessageReceived(data.message)
+    })
+  }, [socket])
+
   return (
     <div className='App'>
-      <input placeholder='Message...'></input>
-      <button>Send Message</button>
+      <input placeholder='Message...' onChange={(event)=>{
+        setMessage(event.target.value)}
+      }
+      ></input>
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Messages:</h1>
+      <p>{messageReceived}</p>
     </div>
   );
 }
