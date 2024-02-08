bce305e421b3e7ff039d558afda9c01ce472913b
Miruo Lin <miruomoo@gmail.com>
2024-02-08 12:59:18 -0500

    Created Room feature

--- client/pokercow/src/App.js
+++ client/pokercow/src/App.js
@@ -5,27 +5,43 @@ import { useEffect, useState } from 'react';
 const socket = io.connect('http://localhost:3001');
 
 function App() {
-
+  const [room, setRoom] = useState("");
+  const [inRoom, setInRoom] = useState(false);
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
-  function sendMessage(){
-    socket.emit("send_message", {message});
+
+  function sendMessage() {
+    socket.emit("send_message", { message, room });
   }
 
-  useEffect(()=>{
-    socket.on("receive_message", (data)=>{
+  function joinRoom() {
+    socket.emit("join_room", room);
+    setInRoom(true);
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
       setMessageReceived(data.message)
     })
   }, [socket])
 
   return (
     <div className='App'>
-      <input placeholder='Message...' onChange={(event)=>{
-        setMessage(event.target.value)}
+      {!inRoom && <div className='room-input'>
+        <input placeholder='Enter Room Number'
+          onChange={(event) => {
+            setRoom(event.target.value)
+          }}></input>
+        <button onClick={joinRoom}>Join</button>
+      </div>}
+      {inRoom && <h1>Room: {room}</h1>}
+      <input placeholder='Message...' onChange={(event) => {
+        setMessage(event.target.value)
+      }
       }
       ></input>
       <button onClick={sendMessage}>Send Message</button>
-      <h1>Messages:</h1>
+      <h1>Message:</h1>
       <p>{messageReceived}</p>
     </div>
   );
