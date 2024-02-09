13654d30297ca270984b6a42193979af3931d583
Miruo Lin <miruomoo@gmail.com>
2024-02-09 16:11:19 -0500

    Added cards and fixed chatbox

--- client/pokercow/src/components/Game.js
+++ client/pokercow/src/components/Game.js
@@ -1,13 +1,29 @@
 
-import Table from "../assets/Table.png";
-
+import { useState } from "react";
+import table from "../assets/Table.png";
+import handback from "../assets/handback.png";
 import "../styles/Game.css";
+import twos from "../assets/cards/2s.png";
+import sevenh from "../assets/cards/7h.png";
+
 
 function Game({ socket, room, messagesReceived, setMessagesReceived, message, setMessage, inRoom, playerName }) {
 
-    function sendMessage() {
+    const [peek, setPeek] = useState(false);
+
+    function peekHand() {
+        setPeek(true);
+    }
+
+    function hideHand() {
+        setPeek(false);
+    }
+
+    function sendMessage(event) {
+        event.preventDefault();
         socket.emit("send_message", { message, room, playerName });
-        // setMessagesReceived([...messagesReceived, `${playerName} : ${message}`])
+        setMessagesReceived([...messagesReceived, `${playerName} : ${message}`]);
+        setMessage("");
     }
 
     return (
@@ -15,20 +31,28 @@ function Game({ socket, room, messagesReceived, setMessagesReceived, message, se
             {inRoom && <div>
                 <h1>Room: {room}</h1>
                 <div className="chatroom">
-                    < h1 > Messages:</h1 >
+                    <h1 className="message-title"> Messages:</h1 >
                     <div className="chatbox">
-                        {messagesReceived.length ? messagesReceived.map((message) =>
-                            (<p>{message}</p>)
+                        {messagesReceived.length ? messagesReceived.map((message, index) =>
+                            (<p className="message" key={index}>{message}</p>)
                         ) : <p></p>}
                     </div>
-                    <input className="text-input" placeholder='Message...' onChange={(event) => {
-                        setMessage(event.target.value)
-                    }
-                    }
-                    ></input>
-                    <button onClick={sendMessage}>Send Message</button>
+                    <form onSubmit={sendMessage}>
+                        <input className="text-input" placeholder='Message...' onChange={(event) => {
+                            setMessage(event.target.value)
+                        }
+                        } value={message}
+                        ></input>
+                        <button type="submit">Send Message</button>
+                    </form>
                 </div>
-                <img className="pokertable" src={Table} alt="pokertable"></img>
+                <img className="pokertable" src={table} alt="pokertable"></img>
+                {!peek && <img className="handback" src={handback} onMouseEnter={peekHand}
+                ></img>}
+                {peek && <div className="hand" onMouseLeave={hideHand}>
+                    <img className="card" src={twos}></img>
+                    <img className="card" src={sevenh}></img>
+                </div>}
             </div>}
         </>
     )
